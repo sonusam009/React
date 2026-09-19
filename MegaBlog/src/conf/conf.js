4ce43a1ed0626b129094a14b205be6036d5f0f38
Sonu Parmar <sonusam009@gmail.com>
2026-09-19 14:32:56 +0530

    feat: integrate TinyMCE API key from configuration in RTE component

--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -3,8 +3,8 @@ const conf = {
     appwriteProjectId: String(import.meta.env.VITE_APWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteTinyMCEAPIKey : String(import.meta.VITE_APPWRITE_TINYMCE_API_KEY)
 }
 
 export default conf
